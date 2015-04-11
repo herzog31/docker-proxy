e4cc6a8ef09202523d548c6649bec41c75e69de7
Mark J. Becker <mjb@marbec.com>
2015-04-11 16:47:16 +0200

    Gather container names and public ports

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
 FROM python:3.4-onbuild
 
+ENV PROXY_BASE_URL="docker.marb.ec"
+
 CMD [ "python" , "./main.py" ]
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+import os
 from threading import Thread
 from docker import Client
 import json
 
+sockUrl = "unix:///var/run/docker.sock"
+
 class MonitorThread(Thread):
-    def __init__(self, sock):
+    def __init__(self, app, sock):
         super(MonitorThread, self).__init__()
 
+        self.app = app
         self.sock = sock
         self.cli = Client(base_url=self.sock)
 
@@ -14,13 +18,31 @@ class MonitorThread(Thread):
             event = json.loads(event.decode('utf-8'))
             print(json.dumps(event, indent=4))
 
+            self.app.updateProxy()
+
     def stop(self):
         self.cli.close()
 
-def main():
-    sockUrl = "unix:///var/run/docker.sock"
+class App():
 
-    MonitorThread(sockUrl).start()
+    def __init__(self, sock, baseUrl):
+        self.sock = sock
+        self.baseUrl = baseUrl
+        self.cli = Client(base_url=self.sock)
+        self.monitor = MonitorThread(self, sockUrl).start()
+
+    def updateProxy(self):
+        for container in self.cli.containers(all=True):
+            ports = container.get("Ports")
+            publicPort = None
+            for port in ports:
+                if "PublicPort" in port:
+                    publicPort = port.get("PublicPort")
+            if publicPort is not None:
+                name = container.get("Names")[0]
+                name = name.replace('/', '')
+                print("Container with name " + name + " is reachable at port " + str(publicPort) + " -> http://" + name + "." + self.baseUrl + "/")
 
 if __name__ == "__main__":
-    main()
+    baseUrl = os.getenv("PROXY_BASE_URL", "marb.ec")
+    app = App(sockUrl, baseUrl)
